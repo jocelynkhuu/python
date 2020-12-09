9dfc4c29811ec1d0645753c603ca02d00d77129d
ohthedeception <62637533+ohthedeception@users.noreply.github.com>
2020-12-08 20:45:03 -0800

    Update shared_email_access.py

--- shared_email_access.py
+++ shared_email_access.py
@@ -1,3 +1,6 @@
+# This script was created to help with generating powershell cmds to add users to shared emails. Cmdlet normally does not take multiple users so each line had to be generated manually.
+# With this script, I am able to input in emails and have the output the cmds to copy and paste.
+
 #!/usr/bin/python3
 
 import re 
@@ -8,4 +11,4 @@ user = input("User(s) email addresses that need access? ")
 emails = re.findall(r'[\w\.-]+@[\w\.-]+\.\w+', user)
 
 for addresses in emails:
-    print(f"Add-MailboxFolderPermission -Identity {username}:\calendar -User {addresses} -AccessRights Editor -SharingPermissionFlags None")
+    print(f"Add-MailboxFolderPermission -Identity {username}@email.com -User {addresses} -AccessRights Editor -SharingPermissionFlags None")
