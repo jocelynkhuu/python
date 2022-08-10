e871d705fad3b212a34cffa092cc18429256d285
jocelynkhuu <62637533+jocelynkhuu@users.noreply.github.com>
2022-08-10 13:30:51 -0400

    Update compare_uid_lists.py

--- compare_uid_lists.py
+++ compare_uid_lists.py
@@ -6,24 +6,36 @@
 import sys
 import subprocess
 
+admin_pass = getpass('Enter admin password: ')
+ldap_server = 'ldaps://example.com'
 base = 'dc=example,dc=com'
-user_dn = f'cn=user,{base}'
-corp_ldap_server = 'ldaps://example.network'
+people_base = f'ou=example,{base}'
+rootDN = f'cn=user,{base}'
+
+con = ldap.initialize(ldap_server)
+con.bind_s(rootDN, admin_pass)
 
 prod_list = []
 
-# List is pasted here as an example for if you don't have access to prod ldap and get a list from someone else
 print("Paste in prod ldap list and hit enter then CTRL+D to save.")
 prod_contents = sys.stdin.readlines()
 for item in prod_contents:
     uid = item.split()[-1]
     prod_list.append(uid)
 
-# If you have access to corp ldap then can connect directly using creds to pull list of uids
-print("Enter in admin password to pull corp ldap list")
-corp_users = subprocess.check_output(f"ldapsearch -LLL -xD {user_dn} -b {base} -H {corp_ldap_server} -W '(uid=*)' uid | grep -v '^dn: ' | sed 's/uid: //g' | sort", shell=True)
-corp_decode = corp_users.decode('utf-8')
-corp_list = corp_decode.splitlines()
+print("Comparing prod LDAP to corp LDAP")
+full_corp_list = []
+corp_list = []
+
+filterstr = 'objectClass=posixaccount'
+attrlist = ['uid']
+results = con.search_s(people_base, ldap.SCOPE_SUBTREE, filterstr=filterstr, attrlist=attrlist)
+for item in results:
+    full_corp_list.append(item[1])
+for uid in full_corp_list:
+    uid = uid.get('uid')
+    uid = uid[0].decode('utf-8')
+    corp_list.append(uid)
 
 missing_corp = list(set(prod_list).difference(corp_list))
 missing_prod = list(set(corp_list).difference(prod_list))
