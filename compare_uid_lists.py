8724125810b925255afda4e945857eb7a95250eb
jkhuu <62637533+jocelynkhuu@users.noreply.github.com>
2023-01-02 05:45:47 -0500

    Update compare_uid_lists.py

--- compare_uid_lists.py
+++ compare_uid_lists.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Script to check 2 lists from prod and corp ldap groups 
+# Script to check 2 lists from 2 different ldap groups 
 # Allows taking input from user in format of memberUID: first.lastname
 
 import os
@@ -17,35 +17,35 @@ rootDN = f'cn=user,{base}'
 con = ldap.initialize(ldap_server)
 con.bind_s(rootDN, admin_pass)
 
-prod_list = []
+second_list = []
 
-print("Paste in prod ldap list and hit enter then CTRL+D to save.")
-prod_contents = sys.stdin.readlines()
-for item in prod_contents:
+print("Paste in first ldap list and hit enter then CTRL+D to save.")
+second_contents = sys.stdin.readlines()
+for item in second_contents:
     uid = item.split()[-1]
-    prod_list.append(uid)
+    second_list.append(uid)
 
-print("Comparing prod LDAP to corp LDAP")
-full_corp_list = []
-corp_list = []
+print("Comparing first LDAP to second LDAP group")
+full_first_list = []
+first_list = []
 
 filterstr = 'objectClass=posixaccount'
 attrlist = ['uid']
 results = con.search_s(people_base, ldap.SCOPE_SUBTREE, filterstr=filterstr, attrlist=attrlist)
 for item in results:
-    full_corp_list.append(item[1])
-for uid in full_corp_list:
+    full_first_list.append(item[1])
+for uid in full_first_list:
     uid = uid.get('uid')
     uid = uid[0].decode('utf-8')
-    corp_list.append(uid)
+    first.append(uid)
 
-missing_corp = list(set(prod_list).difference(corp_list))
-missing_prod = list(set(corp_list).difference(prod_list))
-matches = list(set(prod_list).intersection(corp_list))
+missing_first = list(set(second_list).difference(first_list))
+missing_second = list(set(first_list).difference(second_list))
+matches = list(set(second_list).intersection(first_list))
 
-print("THESE UIDs ARE IN PROD BUT NOT CORP:")
-print(missing_corp)
-#print("THESE UIDs ARE IN CORP BUT NOT PROD:")
-#print(missing_prod)
-#print("THESE UIDS ARE IN BOTH CORP AND PROD:")
+print("THESE UIDs ARE IN SECOND BUT NOT FIRST:")
+print(missing_first)
+#print("THESE UIDs ARE IN FIRST BUT NOT SECOND:")
+#print(missing_SECOND)
+#print("THESE UIDS ARE IN BOTH FIRST AND SECOND:")
 #print(matches)
