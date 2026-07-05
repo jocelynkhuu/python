5f12553ae0f6d0771ab58af7015ae46d1374fbee
Jocelyn Khuu <jocelynkhuu@users.noreply.github.com>
2026-07-05 00:13:59 -0700

    update script

--- download_pdfs.py
+++ download_pdfs.py
@@ -4,8 +4,14 @@
 
 import requests
 
-for i in range (1,31): # 30 chapters 
-    r = requests.get(f'https://link/to/pdfs/{i:02d}.pdf') # chapters are named the the same but numbered differently
-    file = open(f"~/Downloads/name/of/pdf/{i:02d}.pdf", 'wb')
-    file.write(r.content)
-    file.close()
+for i in range (1,28):
+    file_name = f"{i:02d}.pdf"
+
+    print(f"Downloading {file_name}")
+    r = requests.get(f"https://link/to/pdf/here/{i:02d}.pdf")
+
+    if r.status_code == 200:
+        with open(file_name, "wb") as f:
+            f.write(r.content)
+    else:
+        print(f"Errored with status code: {r.status_code}")
